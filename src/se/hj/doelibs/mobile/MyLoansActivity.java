f087de355968a4ff8e43da9b5e2e2a37a95a0eed
yaster77 <yaster77@gmail.com>
2014-11-26 13:53:23 +0100

    Added ListAdapters for Loans and Reservation. Added ListViews for Loans and Reservation. Changed activity_my_loans.xml to support lists. Changed MyLoansActivity.java so it will fill in the current loans and reservations into the lists. Added string values inside string.xml

--- src/se/hj/doelibs/mobile/MyLoansActivity.java
+++ src/se/hj/doelibs/mobile/MyLoansActivity.java
@@ -7,14 +7,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import se.hj.doelibs.api.LoanDao;
 import se.hj.doelibs.api.ReservationDao;
+import se.hj.doelibs.mobile.listadapter.LoanListAdapter;
+import se.hj.doelibs.mobile.listadapter.ReservationListAdapter;
+import se.hj.doelibs.model.Loan;
 import se.hj.doelibs.model.Reservation;
 
 import java.util.List;
 
 public class MyLoansActivity extends BaseActivity {
+
+	ListView lv_myLoans;
+	ListView lv_myReservations;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,10 @@ public class MyLoansActivity extends BaseActivity {
 	    
 	    View contentView = inflater.inflate(R.layout.activity_my_loans, null, false);
 	    drawerLayout.addView(contentView, 0);
+
+		lv_myLoans = (ListView) findViewById(R.id.loans_list);
+		lv_myReservations = (ListView) findViewById(R.id.reservations_list);
+
 
 		//check if user is logged in
 		if(getCredentials() == null) {
@@ -34,7 +46,7 @@ public class MyLoansActivity extends BaseActivity {
 			final LoanDao loanDao = new LoanDao(getCredentials());
 			final ReservationDao reservationDaoDao = new ReservationDao(getCredentials());
 
-			tv.setText("loading users loans/reservations");
+			//tv.setText("loading users loans/reservations");
 
 			new AsyncTask<Void, Void, List<Reservation>>() {
 
@@ -42,13 +54,33 @@ public class MyLoansActivity extends BaseActivity {
 				protected List<Reservation> doInBackground(Void... params) {
 					//return loanDao.getCurrentUsersLoans();
 					return  reservationDaoDao.getCurrentUsersReservations();
+
 				}
 
 				@Override
 				protected void onPostExecute(List<Reservation> reservations) {
-					tv.setText("user has " + reservations.size() + " reservations");
+					//tv.setText("user has " + reservations.size() + " reservations");
+					lv_myReservations.setAdapter(new ReservationListAdapter(MyLoansActivity.this, reservations ));
 				}
 			}.execute();
+
+			new AsyncTask<Void, Void, List<Loan>>() {
+
+				@Override
+				protected List<Loan> doInBackground(Void... params) {
+					return loanDao.getCurrentUsersLoans();
+				}
+
+				@Override
+				protected void onPostExecute(List<Loan> loans) {
+					//tv.setText("user has " + loans.size() + " reservations");
+					lv_myLoans.setAdapter(new LoanListAdapter(MyLoansActivity.this, loans));
+				}
+			}.execute();
+
+
 		}
+		//lv_myReservations.setAdapter(new ReservationListAdapter(MyLoansActivity.this, reservationDaoDao.getCurrentUsersReservations()));
 	}
+
 }
